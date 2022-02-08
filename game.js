acc75874424c8fbd79e583ca8101f816b307c328
Aleksandra Haniok <a.haniok@gmail.com>
2022-02-08 16:13:23 +0000

    Fetch API to load questions from local JSON file

--- game.js
+++ game.js
@@ -11,34 +11,20 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestion = [];
 
-/* Each question is an object which contains a question, choices 1-4 and a correct answer*/
-let questions = [
-    {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
-];
+let questions = [];
+
+fetch('questions.json')
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 
 const CORRECT_BONUS = 10; // How much is it worth if you quess the answer
 const MAX_QUESTIONS = 3; // How many questions does the user get before they finish
@@ -103,5 +89,3 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 };
-
-startGame();
